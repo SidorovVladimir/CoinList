b8221e16ff7c13964fafa7d89cc94ea939f85fd3
Sidorov Vladimir <v.sidorov29091988@gmail.com>
2024-07-08 21:38:41 +0700

    Create Auth module, Create methods login and register, Create Jwt guard and jwt strategy, Create methods update user and delete user

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -2,7 +2,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from './models/user.model';
 import * as bcrypt from 'bcrypt';
-import { CreateUserDTO } from './dto';
+import { CreateUserDTO, UpdateUserDTO } from './dto';
 import { AppError } from 'src/common/constants/errors';
 // import { users } from 'src/moks/index';
 
@@ -31,5 +31,23 @@ export class UsersService {
       password: dto.password
     });
     return dto;
+  }
+
+  async publicUser(email: string) {
+    return this.userRepository.findOne({
+      where: { email },
+      attributes: { exclude: ['password']},
+    })
+  }
+
+  async updateUser(email: string, dto: UpdateUserDTO): Promise<UpdateUserDTO> {
+    await this.userRepository.update(dto, { where: { email } })
+    return dto;
+  }
+
+  async deleteUser(email: string) {
+    await this.userRepository.destroy({ where: { email } })
+    return true;
+
   }
 }
