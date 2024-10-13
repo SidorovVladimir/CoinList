4ddde50a6dfcb97ad579da846afff80338751ccf
Vladimir Sidorov <v.sidorov29091988@gmail.com>
2024-10-13 21:03:04 +0700

    init project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Home from './components/home';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from './components/auth/login';
+import PrivateRoute from './utils/router/privateRoute';
+
+function App() {
+	return (
+		<div className='App'>
+			<Routes>
+				<Route element={<PrivateRoute />}>
+					<Route path='/' element={<Home />} />
+				</Route>
+				<Route path='login' element={<LoginPage />} />
+			</Routes>
+		</div>
+	);
+}
+
+export default App;
