dce24bca797a075cff2ccbeb32169fd436804d6b
Vladimir Sidorov <v.sidorov29091988@gmail.com>
2024-11-07 22:05:08 +0700

    Add responsive top bar - Refactoring top bar component

--- frontend/src/components/top-bar/styles.ts
+++ frontend/src/components/top-bar/styles.ts
@@ -6,13 +6,17 @@ export const useStyles = makeStyles((theme: Theme) => {
   const colors = tokens(theme.palette.mode);
   return {
     root: {
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      padding: '32px 24px',
-      backgroundColor: `${colors.primary.DEFAULT}`,
-      maxHeight: '95px',
+      background: `${colors.primary.DEFAULT} !important`,
       borderBottom: `1px solid ${colors.borderColor}`,
+      boxShadow: 'none !important',
+    },
+    toolbar: {
+      justifyContent: 'space-between',
+      padding: '25px 45px',
+    },
+    menuIcon: {
+      marginRight: '10px',
+      cursor: 'pointer',
     },
     iconThemeBlock: {
       paddingTop: '10px',
@@ -28,6 +32,7 @@ export const useStyles = makeStyles((theme: Theme) => {
     },
     searchBlock: {
       display: 'flex',
+      maxHeight: '45px',
       borderRadius: '8px',
       marginLeft: '28px',
       backgroundColor: `${colors.primary[600]}`,
