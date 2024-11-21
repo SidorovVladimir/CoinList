0f08b78c48b1ffab694c9cbdcf402ebc621a4edb
Vladimir Sidorov <v.sidorov29091988@gmail.com>
2024-11-21 21:25:11 +0700

    Add line chart - Create line chart component - Add real data to chart - Customization chart

--- frontend/src/common/types/assets/index.ts
+++ frontend/src/common/types/assets/index.ts
@@ -1,3 +1,16 @@
 export interface IAreaChartProps {
   data: Array<number[]>;
 }
+
+export interface ILineChartProps {
+  data: any;
+}
+
+// interface SingleAsset {}
+
+export interface IChartData {
+  name: string;
+  price_chart_data: Array<number[]>;
+  // singleAsset: SingleAsset[];
+  singleAsset: any;
+}
