a954014a34874fbc5677b3b56322d281220d77a4
Vladimir Sidorov <v.sidorov29091988@gmail.com>
2024-12-01 00:32:45 +0700

    Refactor app - Create docker container main

--- src/configurations/index.ts
+++ src/configurations/index.ts
@@ -1,10 +1,10 @@
 export default () => ({
   port: process.env.PORT,
-  db_port: process.env.DB_PORT,
-  db_user: process.env.DB_USER,
-  db_password: process.env.DB_PASSWORD,
-  db_host: process.env.DB_HOST,
-  db_name: process.env.DB_NAME,
+  db_port: process.env.POSTGRES_PORT,
+  db_user: process.env.POSTGRES_USER,
+  db_password: process.env.POSTGRES_PASSWORD,
+  db_host: process.env.POSTGRES_HOST,
+  db_name: process.env.POSTGRES_DB,
   secret_jwt: process.env.SECRET,
   expire_jwt: process.env.EXPIRE_JWT,
 });
